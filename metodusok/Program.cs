1c6943bbfd75e88ea9d28f900883c511151c37c0
aprobence <aprobence2017@gmail.com>
2024-11-05 09:31:58 +0100

    2. óra anyaga

--- metodusok/Program.cs
+++ metodusok/Program.cs
@@ -6,7 +6,16 @@
 		{
 			Koszonto();
             Console.WriteLine(Osszeadas(10,90));
-			Console.ReadLine();
+			int a = 5;
+            Console.WriteLine(Duplaz(a));
+            Console.WriteLine(a);
+			int duplazott = Duplaz(a);
+            Console.WriteLine(duplazott);
+            Console.WriteLine(Osszeg(10,7));
+			int elso = 5;
+			int masodik = 10;
+			Csere(elso,masodik);
+            Console.ReadLine();
 		}
 		static void Koszonto()
 		{
@@ -22,5 +31,22 @@
 			return a + b;
 		}
 
+		static int Duplaz(int szam)
+		{
+			return szam*2;
+		}
+
+		static int Osszeg(int a, int b=5, int c = 2)
+		{
+			return a + b + c;
+		}
+
+		static void Csere(int a, int b)
+		{
+			int temp = a;
+			a = b;
+			b = temp;
+			Console.WriteLine(a + " " + b);
+		}
 	}
 }
